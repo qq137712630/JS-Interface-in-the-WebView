a647e23e3b168d4c823ea2d2779829eed8710ae8
qq137712630 <1525626676@qq.com>
2015-02-05 18:33:26 +0800

    1

---
+++ gen/jsinterfacesample/android/chrome/google/com/jsinterface_example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jsinterfacesample.android.chrome.google.com.jsinterface_example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
